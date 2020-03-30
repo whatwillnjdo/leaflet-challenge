bb555ed88c4f4fd7b165a51a0a0e65648f068b76
NJ <nj@Nirmals-MBP.zyxel.com>
2020-03-30 17:36:09 -0400

    NJ - Added the base map and mapped the earthquake data

---
+++ Leaflet-Step-1/README.md
@@ -0,0 +1,2 @@
+# whatwillnjdo
+leaflet-challenge
---
+++ Leaflet-Step-1/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const MapBox_API_KEY = "sk.eyJ1Ijoid2hhdHdpbGxuamRvIiwiYSI6ImNrODZpNTludjBpMDYzZmxrYzF2aXl3dzEifQ.AMTYQW42GREArnlPkm1cQQ";
